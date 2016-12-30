5f27876a967630b10770486296b628e79e91b58f
xDSUP <ilya.elfimow@yandex.ru>
2016-12-30 12:38:22 +0300

    PEP8, поправлены названия,исправлены функции

--- duplicates.py
+++ duplicates.py
@@ -1,41 +1,35 @@
 import os
-from os.path import join, getsize
+from copy import copy
 
-def get_info_files(main_folder):#Возвращает путь,имена и размер всех фаилов в этой директории
-    roots = []; names = []; sizes=[]
-    for root ,dirs, files in os.walk(main_folder): 
+print_txt = "Имя файла: {n}   Путь к файлу: {p}"
+
+
+def get_info_files(main_folder):  
+    paths = []
+    names_and_size = []
+    for root, dirs, files in os.walk(main_folder):
         for file in files:
-            roots.append(root)
-            names.append(file)
-            sizes.append(getsize(join(root, file)))
-    return roots,names,sizes
+            paths.append(root)
+            names_and_size.append([file, os.path.getsize(root+'\\'+file)])
+    return paths, names_and_size
 
 
-def are_files_duplicates(roots, names, sizes):#Ищет дубликаты,возвращает найденные
-    overlooked = []
+def get_files_duplicates(paths, names_and_size):
     duplicates = []
-    for file_1 in range(len(names)):
-        for file_2 in range(len(names)):
-            if (file_1 != file_2) and (overlooked.count(file_1) == 0) and (overlooked.count(file_1) == 0) and (names[file_1] == names[file_2]) and (sizes[file_1] == sizes[file_2]):
-                overlooked.append(file_1)
-                overlooked.append(file_2)
-                duplicates.append([file_2, file_1])
+    names = copy(names_and_size)  # для исключения повторных индексов
+    for file in names_and_size:
+        if names_and_size.count(file) > 1:
+            index = names.index(file)
+            duplicates.append([names_and_size[index][0], paths[index]])
+            names[index] = None
     return duplicates
 
-def print_result(duplicates,roots, names, sizes):
-    for file in duplicates:
-        print('Файл с именем %s, размером %dкб, находящийся в директории %s' % (names[file[0]], sizes[file[0]], join(roots[file[0]], names[file[0]])))
-        print('является дубликатом файла с именем %s, размером %dкб, находящимся в директории %s' % (names[file[1]], sizes[file[1]], join(roots[file[1]], names[file[1]])))
-        print('----------------------------------------------------------------------------------------------')
-
-        
-    
 if __name__ == '__main__':
-    path_name = input("Введите путь до директории,для поиска похожих фаилов:  ")
-    files_info = get_info_files(path_name)
-    duplicates = are_files_duplicates(*files_info)
+    path_name = input("Введите путь до директории,для поиска похожих фаилов: ")
+    files_info = get_info_files(path_name) # Вернет пути, имена, размеры
+    duplicates = get_files_duplicates(*files_info)  # Вернет инфо по дубликатам
     if duplicates:
-        print_result(duplicates, *files_info)
+        for duplicate in sorted(duplicates):
+            print(print_txt.format(n=duplicate[0], p=duplicate[1]))
     else:
         print("Дубликатов необнаружено")
-    
